141351adffd45ee760f2eebd6b82d343e981b4a8
DokaDev <awesome_devnet@outlook.com>
2024-01-25 22:33:33 +0900

    feat: ogg file test

---
+++ ConsolePlayer/Program.cs
@@ -0,0 +1,38 @@
+﻿using Core;
+namespace ConsolePlayer {
+    internal class Program {
+        [STAThread]
+        static void Main(string[] args) {
+            MediaCore core = new();
+            core.SetMedia("getout.ogg");
+
+            while(true) {
+                Console.WriteLine("Play : [Enter]");
+                Console.WriteLine("PauseResume/Resume : [Space]");
+                Console.Write(": ");
+                ConsoleKeyInfo c = Console.ReadKey();
+
+                switch(c.Key) {
+                    case ConsoleKey.Enter:
+                        core.Play();
+                        Console.WriteLine("Start Playing");
+                        break;
+                    case ConsoleKey.Spacebar:
+                        core.PauseResume();
+                        Console.WriteLine("PauseResume / Resume");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void Play() {
+
+        }
+
+        private void Pause() {
+
+        }
+    }
+}
---
+++ Core/MediaCore.cs
@@ -0,0 +1,36 @@
+﻿using IrrKlang;
+
+namespace Core {
+    public class MediaCore {
+        private ISoundEngine _engine;
+        private ISound? _currentMedia;
+
+        public string? Path { get; set; }
+        public bool IsPlaying { get; private set; } = false;
+
+        /// <summary>
+        /// CONSTRUCTOR
+        /// DESC: INIT SOUND ENGINE
+        /// </summary>
+
+        public MediaCore() {
+            _engine = new();
+        }
+
+        //[STAThread]
+        public void SetMedia(string path) {
+            Path = path;
+            if(!File.Exists(path)) throw new FileNotFoundException($"{path} not exist");
+        }
+
+        public void Play() {
+            if(_currentMedia != null) _currentMedia.Stop();
+            _currentMedia = _engine.Play2D(Path, true);
+            IsPlaying = true;
+        }
+
+        public void PauseResume() {
+            if(_currentMedia != null) _currentMedia.Paused = !_currentMedia.Paused;
+        }
+    }
+}
